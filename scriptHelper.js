e6aa67f38721b8400e789381f7a763aa9a17d4fc
Tori Rodgers <torirodgers22@gmail.com>
2024-08-24 20:22:29 -0500

    added addDestinationInfo function

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,19 +3,18 @@
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
- }
+    const missionTarget = document.getElementById("missionTarget");
+    
+    missionTarget.innerHTML = `<h2>Mission Destination</h2>
+                            <ol>
+                                <li>Name: ${name} </li>
+                                <li>Diameter: ${diameter} </li>
+                                <li>Star: ${star}</li>
+                                <li>Distance from Earth: ${distance} </li>
+                                <li>Number of Moons: ${moons} </li>
+                            </ol>
+                            <img src="${imageUrl}">`;
+}
   
  function validateInput(testInput) {
     if (testInput === '') {
